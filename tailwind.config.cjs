80036f59c9a462d51999d2d663480edc68cd776d
Harshith Iyer <harbar2021@gmail.com>
2023-10-06 19:50:02 -0400

    done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
+import plugin from "tailwindcss";
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        plugin(({ addBase, theme }) => {
+            addBase({
+                h1: { fontSize: theme("fontSize.2xl") },
+                h2: { fontSize: theme("fontSize.xl") },
+                h3: { fontSize: theme("fontSize.lg") },
+            });
+        }),
+    ],
 };
